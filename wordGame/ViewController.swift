4a0a6168e17541256a9c1b682438e461fd2567bf
West Kraemer <kraemerwest@gmail.com>
2019-07-01 12:12:11 +0200

    first commit for wordgame

---
+++ wordGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  wordGame
+//
+//  Created by West Kraemer on 7/1/19.
+//  Copyright © 2019 West Kraemer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
